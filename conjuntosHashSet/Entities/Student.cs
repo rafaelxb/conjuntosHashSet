f8422a3e288c7059873b6f1f85ca28769fc95569
rafaelxb <rfxbarbosa@gmail.com>
2023-03-25 11:19:37 -0300

    Adicionar arquivos de projeto.

---
+++ conjuntosHashSet/Entities/Student.cs
@@ -0,0 +1,29 @@
+﻿namespace conjuntosHashSet.Entities
+{
+    class Student
+    {
+        public int StudentNumber { get; set; }
+
+        public Student(int studentNumber)
+        {
+            StudentNumber = studentNumber;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (!(obj is Student))
+            {
+                return false;
+            }
+
+            Student objCourse = (Student)obj;
+
+            return StudentNumber.Equals(objCourse.StudentNumber);
+        }
+
+        public override int GetHashCode()
+        {
+            return StudentNumber.GetHashCode();
+        }
+    }
+}
---
+++ conjuntosHashSet/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using conjuntosHashSet.Entities;
+
+class Program
+{
+    private static void Main(string[] args)
+    {
+        HashSet<Student> a = new HashSet<Student>();
+        HashSet<Student> b = new HashSet<Student>();
+        HashSet<Student> c = new HashSet<Student>();
+
+        int cont;
+        string[] courses = new string[] {"A","B","C"}; 
+        
+        foreach(string course in courses)
+        {
+            Console.Write($"How many students for course {course}? ");
+            cont = int.Parse(Console.ReadLine());
+            for(int i = 0; i < cont; i++)
+            {
+                Student student = new Student(int.Parse(Console.ReadLine()));
+                if (course.Equals("A"))
+                {
+                    a.Add(student);
+                } else if (course.Equals("B"))
+                {
+                    b.Add(student);
+                } else if (course.Equals("C"))
+                {
+                    c.Add(student);
+                }
+            }
+
+        }
+
+        HashSet<Student> allStudents = new HashSet<Student>(a);
+
+        allStudents.UnionWith(b);
+        allStudents.UnionWith(c);
+
+        Console.WriteLine("Total Students: " + allStudents.Count);
+
+        
+        
+
+
+
+    }
+}
